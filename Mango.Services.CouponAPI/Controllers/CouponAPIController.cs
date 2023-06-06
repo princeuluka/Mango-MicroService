ddca1bcbb8fbc16dfe927f28831f2f95c1ad04a7
Prince <Ulukaprince1996@gmail.com>
2023-06-05 21:02:53 -0400

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,48 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex )
+            {
+
+                return ex.Message;
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                return obj;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+    }
+}
